fe4ff05d4a97254ce556a4386b5ca2c294fad4c8
Sean Hoops <50555459+SeanathonHooper@users.noreply.github.com>
2026-08-05 20:25:49 -0400

    main project files

---
+++ src/main/java/seanathonhooper/pulverize/mixin/ItemEntityMixin.java
@@ -0,0 +1,46 @@
+package seanathonhooper.pulverize.mixin;
+
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import seanathonhooper.pulverize.Pulverize;
+
+
+@Mixin(ItemEntity.class)
+public abstract class ItemEntityMixin {
+
+    @Inject(method = "tick", at =@At("TAIL"))
+        private void detectDripstoneBlock(CallbackInfo ci) {
+            ItemEntity itemEntity = (ItemEntity) (Object) this;
+
+            if (itemEntity.level().isClientSide()) return;
+
+
+            if (!itemEntity.getItem().is(Items.STONE) && !itemEntity.getItem().is(Items.GRAVEL) && !itemEntity.getItem().is(Items.COBBLESTONE)) return;
+
+            Pulverize.LOGGER.info("Survived item type check");
+
+            BlockState state = itemEntity.level().getBlockState(itemEntity.getOnPos());
+
+            if (state.is(Blocks.POINTED_DRIPSTONE)) {
+                ItemStack oldStack = itemEntity.getItem();
+
+                if (itemEntity.getItem().is(Items.STONE) || itemEntity.getItem().is(Items.COBBLESTONE)) {
+                    ItemStack newStack = new ItemStack(Items.GRAVEL, oldStack.getCount());
+                    itemEntity.setItem(newStack);
+                }
+                else if (itemEntity.getItem().is(Items.GRAVEL)){
+                    ItemStack newStack = new ItemStack(Items.SAND, oldStack.getCount());
+                    itemEntity.setItem(newStack);
+                }
+            }
+        }
+
+    }
+
